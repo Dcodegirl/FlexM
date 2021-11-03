4b97ea92d36fcb35e9a1ef23d3762c28b385135f
olushesitoheeb <olaolushesi98@gmail.com>
2021-11-03 09:46:40 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ToastProvider } from "react-toast-notifications";
-import history from "./utils/history";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastProvider } from 'react-toast-notifications';
+import history from './utils/history';
 
-import AppRouter from "./router/AppRouter";
+import AppRouter from './router/AppRouter';
 
 const App = () => {
-  useEffect(() => {
-    let isCancelled = false;
+    useEffect(() => {
+        const isSupported = () =>
+            'Notification' in window &&
+            'serviceWorker' in navigator &&
+            'PushManager' in window;
 
-    if (!isCancelled) {
-      (function requestNotification() {
-        if (Notification.permission !== "denied") {
-          Notification.requestPermission();
+        let isCancelled = false;
+
+        if (!isCancelled && isSupported()) {
+            (function requestNotification() {
+                if (Notification.permission !== 'denied') {
+                    Notification.requestPermission();
+                }
+            })();
         }
-      })();
-    }
 
-    return () => {
-      isCancelled = true;
-    };
-  }, []);
+        return () => {
+            isCancelled = true;
+        };
+    }, []);
 
-  return (
-    <ToastProvider>
-      <Router history={history}>
-        <AppRouter />
-      </Router>
-    </ToastProvider>
-  );
+    return (
+        <ToastProvider>
+            <Router history={history}>
+                <AppRouter />
+            </Router>
+        </ToastProvider>
+    );
 };
 
 export default App;
