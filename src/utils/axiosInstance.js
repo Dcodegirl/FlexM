2810e62eb7a3b12821bf2616766412d7eb064f5a
jumzeey <umatonjayrex01@gmail.com>
2023-12-24 23:44:33 +0100

    added live api url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 // Retrieve token from session storage
 const token = sessionStorage.getItem('token');
 
-// Create an Axios instance with a base URL
+// Define base URLs for different environments
+const baseURLs = {
+  local: 'http://localhost:3000/api', // Replace with your local base URL
+  staging: 'https://staging-api.flexdeals.com.ng/api', // Replace with your staging base URL
+  production: 'https://flexmoney.cico.ng/api', // Replace with your production base URL
+};
+
+// Determine the environment (you can set this dynamically based on your build process or other logic)
+const environment = process.env.REACT_APP_ENVIRONMENT || 'local';
+
+// Create an Axios instance with the appropriate base URL
 const axiosInstance = axios.create({
-  baseURL: 'https://stagging-api.flexdeals.com.ng/api', // Replace with your actual base URL
+  baseURL: baseURLs[environment],
   headers: {
     'Content-Type': 'application/json',
     // Add any additional headers if needed
