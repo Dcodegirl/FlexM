2cd1781194de72ff8300f34f840e4ad2c403e60b
jumzeey <umatonjayrex01@gmail.com>
2024-02-09 18:26:02 +0100

    fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// let path = "https://flexmoney.cico.ng/api"
-let path = "https://stagging-api.flexdeals.com.ng/api"
+let path = "https://flexmoney.cico.ng/api"
+// let path = "https://stagging-api.flexdeals.com.ng/api"
 ////TRANSACTIONCOST////
 export const TRANSACTION_COST = `${path}/transaction-costs/Transfer/agents/{amount}`
 //////AUTH///////
