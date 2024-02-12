d1df4ef8b842a19e8266a53930f26cb856dedac3
jumzeey <umatonjayrex01@gmail.com>
2024-02-12 09:29:50 +0100

    fixes

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -8,8 +8,6 @@ const token = sessionStorage.getItem('token');
 const baseURLs = 'https://flexmoney.cico.ng/api'
 // const staggingBaseURLs = 'https://stagging-api.flexdeals.com.ng/api'
 
-// Determine the environment (you can set this dynamically based on your build process or other logic)
-// const environment = process.env.REACT_APP_ENVIRONMENT || 'local';
 
 // Create an Axios instance with the appropriate base URL
 const axiosInstance = axios.create({
