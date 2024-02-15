3bd39886d13492638dd6ba8412402701142eb3bd
Joy Onweani <joyamaka1995@gmail.com>
2024-02-15 11:19:09 +0100

    merged

--- src/features/dashboard/WalletInfo/index.jsx
+++ src/features/dashboard/WalletInfo/index.jsx
@@ -14,7 +14,7 @@ const WalletInfo = ({walletInfoProp}) => {
         <p className="text-deep-green font-medium my-4 text-3xl">{walletInfoProp.title}</p>
       </div>
       <div className="flex flex-col text-[#494343] ">
-        <p className="text-[12px]">Total Balance</p>
+        <p className="text-[12px]">Total Funds Transacted</p>
         <p className="font-medium text-xl">{walletInfoProp.amount}</p>
       </div>
     </div>
